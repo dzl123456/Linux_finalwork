68d1889bf189010d2babbb1575cb90fad29fc736
dzl123456 <645089433@qq.com>
2017-06-28 15:37:09 +0800

    Add files via upload

---
+++ queue.h
@@ -0,0 +1,18 @@
+#include<stdbool.h>
+#ifndef __QUEUE_H_
+#define __QUEUE_H_
+typedef struct queue//定义存储整形的队列
+{
+    int *pBase;//队列头
+    int prev;//队列第一个元素所在位置（一般为0）
+    int rear;//队列最后一个元素的下一个元素所在位置
+    int size;//队列的存储空间
+}*PQUEUE;
+
+void CreateQueue(PQUEUE Q,int maxsize);
+void PrintQueue(PQUEUE Q);
+bool FullQueue(PQUEUE Q);
+bool EmptyQueue(PQUEUE Q);
+bool Enqueue(PQUEUE Q, int val);
+bool Dequeue(PQUEUE Q, int *val);
+#endif
---
+++ stack.h
@@ -0,0 +1,22 @@
+#include<stdbool.h>
+#ifndef __STACK_H_
+#define __STACK_H_
+typedef int elementype;
+//定义存储整形的结点
+typedef struct Node
+{
+    int data;
+    struct Node *pNext;
+}*PNODE;
+//定义存储整形的栈
+typedef struct stack
+{
+    PNODE pTop; //栈顶指针
+    PNODE pBottom;//栈底指针
+}*PSTACK;
+void CreateStack(PSTACK S);
+void PushStack(PSTACK S,int val);
+bool PopStack(PSTACK S,PNODE val);
+void PrintStack(PSTACK S);
+void ClearStack(PSTACK S);
+#endif
